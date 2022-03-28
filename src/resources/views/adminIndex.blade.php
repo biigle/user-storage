fca9bffae108a228181a1eb90cd10478bcc1cc5d
Martin Zurowietz <martin@zurowietz.de>
2022-03-28 16:42:15 +0200

    Fix admin index view mixin

--- src/resources/views/adminIndex.blade.php
+++ src/resources/views/adminIndex.blade.php
@@ -1,14 +1,16 @@
 <?php
-function size_for_humans($size) {
-    $size = intval($size);
-    $unit = '';
-    $units = ['kB', 'MB', 'GB', 'TB'];
-    do {
-        $size /= 1000;
-        $unit = array_shift($units);
-    } while ($size > 1000 && count($units) > 0);
+if (!function_exists('size_for_humans')) {
+    function size_for_humans($size) {
+        $size = intval($size);
+        $unit = '';
+        $units = ['kB', 'MB', 'GB', 'TB'];
+        do {
+            $size /= 1000;
+            $unit = array_shift($units);
+        } while ($size > 1000 && count($units) > 0);
 
-    return round($size, 2).'&nbsp;'.$unit;
+        return round($size, 2).'&nbsp;'.$unit;
+    }
 }
 ?>
 
