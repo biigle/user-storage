f2f145c4456275e784d3ab1b6c2d952316b50da8
aneust <aneustaedter@techfak.uni-bielefeld.de>
2025-03-05 11:10:59 +0100

    Removed spaces

--- src/resources/views/create.blade.php
+++ src/resources/views/create.blade.php
@@ -180,7 +180,7 @@
             Some files <i class="fa fa-info-circle"></i> were skipped during upload. 
             They already exist in another storage request with equal directory name.
         </p>
-        
+
         <p v-cloak v-if="!finished && hasFiles" class="text-muted">
             <span v-if="finishIncomplete">Failed files with a total size of <span v-text="totalSizeFailedFilesForHumans"></span>.</span> 
             <span v-else>Selected files with a total size of <span v-text="totalSizeForHumans"></span>.</span>
