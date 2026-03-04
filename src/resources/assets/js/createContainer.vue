388139795ba3795c52f97f68d3cffb3c01840be8
Leane Schlundt <lhecht@techfak.de>
2026-03-04 08:52:23 +0100

    Use strict comparison for consistency

--- src/resources/assets/js/createContainer.vue
+++ src/resources/assets/js/createContainer.vue
@@ -167,7 +167,7 @@ export default {
             // See: https://github.com/biigle/user-storage/issues/16.
             newFiles = newFiles.map((file) => {
                 let newName = this.removeInvalidCharacters(file.name);
-                this.pathContainsInvalidChar = file.name != newName || this.pathContainsInvalidChar;
+                this.pathContainsInvalidChar = file.name !== newName || this.pathContainsInvalidChar;
                 return this.pathContainsInvalidChar ? new File([file], newName, { type: file.type }) : file;
             });
 
@@ -240,7 +240,7 @@ export default {
             let name = prompt('Please enter the new directory name');
             if (name) {
                 let newName = this.removeInvalidCharacters(name);
-                this.pathContainsInvalidChar = (name != newName) || this.pathContainsInvalidChar;
+                this.pathContainsInvalidChar = (name !== newName) || this.pathContainsInvalidChar;
                 name = this.pathContainsInvalidChar ? newName : name;
                 this.handleNewDirectory(name, root === true);
             }
